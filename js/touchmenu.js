0a620c5c02754effcfcad31fb6c9518b5beaccaf
Justice Yen <justice.yen@gmail.com>
2017-08-10 19:01:38 -0700

    complete default values for menu

--- js/touchmenu.js
+++ js/touchmenu.js
@@ -3,8 +3,20 @@ $(document).ready(() => {
   let $touchmenu = $("#touchmenu");
 
 //opens touch menu, timer to prevent onclick from firing at same time
+//set default values of day/time to clicked element
 
   $("#calendar").on('touchstart', (e) => {
+    let target = e.target;
+    let fields = {day: "", hour: "", minute: "", ampm: ""};
+    if(target.nodeName === "LABEL") {
+      fields = parseID(target.control.id);
+    } else if(target.nodeName ==="INPUT") {
+      fields = parseID(target.id);
+    }
+    $("#startday").val(fields.day);
+    $("#starthour").val(fields.hour);
+    $("#startminute").val(fields.minute);
+    $("#ampmstart").val(fields.ampm); 
     setTimeout(showTouchMenu, 350);
   });
 
@@ -23,6 +35,25 @@ $(document).ready(() => {
 
   function hideTouchMenu() {
     $touchmenu.css("display", "none");
+  }
+
+  function parseID(id) { //function to parse the id
+    let day = id.slice(0,3);
+    let ampm = id.slice(-2);
+    let time = id.slice(3,-2);
+    let hour, minute;
+    if(time.slice(-2) === "30") {
+      minute = "30";
+      hour = parseInt(time.slice(0,-2));
+    } else {
+      minute = "00";
+      hour = parseInt(time);
+    }
+    return {day, hour, minute, ampm};
+  }
+
+  function getID(day, hour, minute, ampm) { //function to return id
+
   }
 
 });
